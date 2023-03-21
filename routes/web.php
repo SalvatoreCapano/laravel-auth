cf46b6e5068392f5eac52969ee43c3d839b4098b
Salvatore Capano <capano_salvatore@libero.it>
2023-03-21 15:36:59 +0100

    Agg. gestione login

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Controllers
+use App\Http\Controllers\Admin\PageController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,18 +17,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Ritorna la view welcome.blade.php se si accede all'url /
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::prefix('admin')
+//     ->name('admin.')
+//     ->middleware(['auth', 'verified'])
+//     ->group(function () {
+//         Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
+//     });
+Route::prefix('admin')
+        ->name('admin.')
+        ->middleware(['auth', 'verified'])
+        ->group(function () {
+    Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('posts', PostController::class);
 });
 
-require __DIR__.'/auth.php';
+Route::middleware('auth')
+    ->controller(ProfileController::class)
+    ->prefix('profile')
+    ->name('profile.')
+    ->group(function () {
+        Route::get('/', 'edit')->name('edit');
+        Route::patch('/', 'update')->name('update');
+        Route::delete('/', 'destroy')->name('destroy');
+    });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+require __DIR__ . '/auth.php';
